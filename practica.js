d39140f8a79c7becfc6ce977d6ccbd9c10a28efe
GLOBANT\eligreg.lopez <Eligreg.M.Lopez.-ND@disney.com>
2021-09-21 13:11:17 -0500

    first commit

---
+++ practica.js
@@ -0,0 +1,117 @@
+// // let firstName = 'Eligreg'
+// // let lastName = 'López'
+// // let username = 'eligregl'
+// // let nickname = 'Elig'
+// // let age = 33
+// // let email = 'eligregl@gmail.com'
+// // let adult = true
+// // let moneySaved = 2000000
+// // let debts = 333333
+
+// // let fullName = `${firstName} ${lastName}` //concatenando 
+// // console.log(fullName)
+// // //`${nombre} ${apellido}`
+// // let realMoney = moneySaved - debts
+// // console.log(realMoney)
+
+
+// // const miFuncion = (firstName, lastName, nickname) => {
+// // 	let fullName = `${firstName} ${lastName}`;
+// // 	return `Mi nombre es ${fullName}, pero prefiero que me digas ${nickname}`;
+// // }
+// // console.log(miFuncion('Luzmary', 'Claro', 'Lucy'));
+
+// // // 
+
+// // const tipoDeSuscripcion = ['Free', 'Basic', 'Expert', 'Expert+'];
+
+// // if (tipoDeSuscripcion.indexOf(tipoDeSuscripcion) === 0 ) {
+// // 	console.log("Solo puedes tomar los cursos gratis");
+// // }
+
+// // if (tipoDeSuscripcion === 'Free') {
+// //        console.log("Solo puedes tomar los cursos gratis");
+// // } else if (tipoDeSuscripcion === 'Basic') {
+// // 	console.log("Puedes tomar casi todos los cursos de Platzi durante un mes");
+// // } else if (tipoDeSuscripcion === 'Expert') {
+// // 	console.log("Puedes tomar casi todos los cursos de Platzi durante un año");
+// // } else if (tipoDeSuscripcion === 'Expert+') {
+// // 	console.log("Tú y alguien más pueden tomar TODOS los cursos de Platzi durante un año");
+// // }
+
+// // // 
+
+// // const cats = ['Bill', 'Jeff', 'Pete', 'Biggles', 'Jasmin'];
+// //     let info = 'My cats are called ';
+
+// //     for(let i = 0; i < cats.length; i++) {
+// // 		info += cats[i] + ', ';
+// //     }
+
+// // 	console.log(info)
+
+// // // 
+
+// // for (let i = 10; i >= 2; i--) {
+// //     console.log("El valor de i es: " + i);
+// // }
+
+// // let i = 10;
+// // while (i >= 2) {
+// // 	i--;
+// // }
+// // console.log("El valor de i es: " + i);
+
+// // 
+
+// // let value_a = 2;
+// // let value_b = 2;
+// // let result = value_a + value_b;
+
+// // let response = parseInt(
+// // 	prompt(` Cuanto es: ${value_a} + ${value_b} `)
+// // );
+
+// // while (response !== result) {
+// // 	if (response !== result) {
+// // 		console.log(`🥺Volvamos a empezar 🥺`);
+// // 		response = parseInt(
+// // 			prompt(` Cuanto es: ${value_a} + ${value_b} `)
+// // 		);
+// // 	}
+// // 	if (response === result) {
+// // 		console.log(`🎉🎉Respuesta correcta 🎉🎉`);
+// // 	}
+// // }
+
+
+// var arr = ["apple", "pear", "tangerine", "lemon", "mango", "pineapple"];
+
+// function firstArrayElement(array) {
+//     console.log(array[0]);
+// }
+
+// firstArrayElement(arr);
+
+// function eachArrayElement(array) {
+//     array.forEach((elem) => {
+//         console.log(elem);
+//     });
+// };
+
+// eachArrayElement(arr);
+
+// var person = {
+//     nombre: 'Luzmary',
+//     apellido: 'Claro',
+//     edad: 28
+// }
+
+// function eachObjectElement(objeto) {
+//     Object.values(objeto).forEach((elem) => {
+//         console.log(elem);
+//     });
+// };
+
+// eachObjectElement(person);
+
